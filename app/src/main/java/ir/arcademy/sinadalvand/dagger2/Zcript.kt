4d16c93d5d2ba29a89fa3d95e84b3cbdcecd34fc
Sina <dalvandsina@yahoo.com>
2019-09-21 00:51:31 +0430

    zSecure added

--- app/src/main/java/ir/arcademy/sinadalvand/dagger2/Zcript.kt
+++ app/src/main/java/ir/arcademy/sinadalvand/dagger2/Zcript.kt
@@ -3,17 +3,25 @@ package ir.arcademy.sinadalvand.dagger2
 import android.util.Log
 import javax.inject.Inject
 
-class Zcript @Inject constructor() {
+class Zcript @Inject constructor(val zsecure: Zsecure) {
+
+    @Inject
+    var zsecure: Zsecure? = null
 
 
-    fun encrypt(str:String):String{
+    fun encrypt(str: String): String {
         return str
     }
 
-    fun decrypt(str:String):String{
-        Log.e("Dagger2","Decrypt run ...")
+    fun decrypt(str: String): String {
+        Log.e("Dagger2", "Decrypt run ...  Key: ${zsecure?.Z_KEY}")
         return str
     }
 
+
+    @Inject
+    fun ZsecureInject(zsecure: Zsecure) {
+        this.zsecure = zsecure
+    }
 
 }
---
+++ app/src/main/java/ir/arcademy/sinadalvand/dagger2/Zsecure.kt
@@ -0,0 +1,9 @@
+package ir.arcademy.sinadalvand.dagger2
+
+import javax.inject.Inject
+
+class Zsecure @Inject constructor() {
+
+    val Z_KEY = "123456789"
+    val Z_IV = "987654321"
+}
